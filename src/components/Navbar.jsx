2941d9454b338063e80571057f6e93ac2ebb76a8
burak-ekinci <bltycxkk@gmail.com>
2024-06-24 18:12:43 +0300

    adding bus time

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light p-1">
-      <a className="navbar-brand honk " href="#">
+      <a className="navbar-brand amatic-sc-bold " href="#">
         KYK YEMEK LISTESI
       </a>
     </nav>
--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -6,6 +6,8 @@ const BaseLayout = ({ props }) => {
     <>
       <Navbar />
       <div className="custom-display">{props}</div>
+      <img className="m-4 img-fluid" src="/bus.jpg" alt="" />
+      <img className="m-4 mt-2 img-fluid" src="/bus1.jpg  " alt="" />
     </>
   );
 };
